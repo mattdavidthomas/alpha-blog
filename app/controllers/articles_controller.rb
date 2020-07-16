4dccba0ca7fe52a563d717f102908494a80f0c5f
mattdavidthomas <MatthewThomas568325@gmail.com>
2020-07-16 17:34:23 +1000

    added edit and update actions and routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,28 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Artcle was created successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Artcle was updates successfully."
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
 end
