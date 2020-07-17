8d0a4704f2c1d130fa4122c9a250407754612e22
mattdavidthomas <MatthewThomas568325@gmail.com>
2020-07-17 22:25:08 +1000

    added user functionality and restrictions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
     end
@@ -14,7 +16,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:notice] = "Artcle was created successfully."
             redirect_to @article
@@ -48,6 +50,12 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] = "You can only edit or delete your own article/s"
+            redirect_to @article
+        end
     end
 
 end
