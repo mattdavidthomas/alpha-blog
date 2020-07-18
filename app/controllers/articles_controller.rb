ae837d61861f5d5a1d7017e00e0ecf6492806225
mattdavidthomas <MatthewThomas568325@gmail.com>
2020-07-18 17:28:05 +1000

    created categories model, show pages, and actions and restricted user access

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,10 +49,10 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-        params.require(:article).permit(:title, :description)
+        params.require(:article).permit(:title, :description, category_ids: [])
     end
     def require_same_user
-        if current_user != @article.user
+        if current_user != @article.user && !current_user.admin?
             flash[:alert] = "You can only edit or delete your own article/s"
             redirect_to @article
         end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_041130) do
+ActiveRecord::Schema.define(version: 2020_07_18_060930) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
